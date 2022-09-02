6c25e913448e15cc3d46acac92cf16d802d050b5
Spencer Brown <spencerbrown07c7@gmail.com>
2022-09-02 11:08:36 +0100

    made some changes

--- load_recording.m
+++ load_recording.m
@@ -1,6 +1,7 @@
 function load_recording(path, recording_channel)
     
     [record, si, recordSegmentHandle] = abfloader(path,'start',0,'stop','e');
+    
     record = transpose(record(:,recording_channel));
     
     samples = 1:length(record);
@@ -8,4 +9,5 @@ function load_recording(path, recording_channel)
     t = ((samples -1)*si)/(10^3);
     
     save('recording.mat','t','record','si')
+    
 end
